93e0c3a452bae36b161c36ca4f7cb2e809ab0768
Germán Méndez <gcpmendez@gmail.com>
2016-01-19 15:26:33 +0000

    Adding Level1 & Level2

--- RotaPelota/Assets/Scripts/finish.cs
+++ RotaPelota/Assets/Scripts/finish.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class finish : MonoBehaviour {
 
-   /* void OnCollisionEnter(Collision colision)
+public class finish : MonoBehaviour
+{
+
+    /* void OnCollisionEnter(Collision colision)
+     {
+         if (colision.relativeVelocity.magnitude >= 0)
+         {
+
+             Debug.Log("la esfera a tocado algo.");
+
+         }
+     } */
+    public TextMesh textObject;
+
+    void Start()
     {
-        if (colision.relativeVelocity.magnitude >= 0)
-        {
 
-            Debug.Log("la esfera a tocado algo.");
-
-        }
-    } */
+        textObject = GameObject.Find("Win").GetComponent<TextMesh>();
+        textObject.text = "";
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Cube")
         {
             Debug.Log("la esfera ha tocado al Cube.");
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+
+        textObject.text = "Win!";
     }
 }
