5082e0e58fc4a602fe2f7f8a73573d0a26443241
Germán Méndez <gcpmendez@gmail.com>
2016-01-19 10:28:55 +0000

    Añadiendo niveles

---
+++ RotaPelota/Assets/StartCountdown.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartCountdown : UnityEngine.MonoBehaviour
+{
+    int time, a;
+    float x;
+    public bool count;
+    public string timeDisp;
+
+    void Start()
+    {
+        GameObject.Find("Bolita").GetComponent<Rigidbody>().useGravity = false;
+        time = 3;
+        count = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (count)
+        {
+            timeDisp = time.ToString();
+
+            TextMesh textObject = GameObject.Find("StartCounter").GetComponent<TextMesh>();
+            textObject.text = timeDisp;
+            x += UnityEngine.Time.deltaTime;
+            a = (int)x;
+            print(a);
+            switch (a)
+            {
+                
+                case 0: textObject.text = "3"; break;
+                case 1: textObject.text = "2"; break;
+                case 2: textObject.text = "1"; break;
+                case 3: textObject.text = "Start!"; break;
+                case 4:
+                    //GameObject.Find("StartCounter").GetComponent<UnityEngine.UI.Text>().enabled = false;
+                    textObject.text = "";
+                    count = false;
+                    GameObject.Find("Bolita").GetComponent<Rigidbody>().useGravity = true;
+                    break;
+            }
+        }
+    }
+}
