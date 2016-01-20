8ddcd5e174ee37c79581e8541c42ada95b4ed7ac
Germán Méndez <gcpmendez@gmail.com>
2016-01-20 13:11:10 +0000

    Finished

---
+++ RotaPelota/Assets/Scripts/dead.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dead : MonoBehaviour {
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	
+
+
+    void  start()
+    {
+        Debug.Log(GameObject.Find("Canica").transform.position.y);
+        if (GameObject.Find("Canica").transform.position.y <= 0)
+        {
+            print("muertee");
+        }
+    }
+
+    void update()
+    {
+
+        Debug.Log(GameObject.Find("Canica").transform.position.y);
+        if (GameObject.Find("Canica").transform.position.y <= 0)
+        {
+            print("muertee");
+        }
+    }
+
+    public void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+    
+}
